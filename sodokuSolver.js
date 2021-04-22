0eb5406f36027150d11cebecde73d5909f7fe865
abassGarane <thelibertinewreck@gmail.com>
2021-04-22 17:59:24 +0300

    Starting commit

---
+++ sodokuSolver.js
@@ -0,0 +1,75 @@
+//create a virtual board
+
+var board=[
+	[0, 5, 1, 3, 6, 2, 7, 0, 0],
+	[0, 4, 0, 0, 5, 8, 0, 0, 0],
+	[0, 0, 0, 4, 0, 0, 0, 2, 5],
+	[0, 8, 0, 0, 0, 0, 9, 0, 3],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[7, 0, 5, 0, 0, 0, 0, 8, 0],
+	[1, 2, 0, 0, 0, 9, 0, 0, 0],
+	[0, 0, 0, 2, 8, 0, 0, 6, 0],
+	[0, 0, 8, 5, 3, 4, 2, 9, 0]
+]
+
+
+//finds spot with zero/empty spot
+function findEmptySpot(board){
+	for(let i = 0; i<9; i++){
+		for(let j=0;j<9; j++){
+			if(board[i][j]===0){
+				return [i,j];
+			}
+		}
+	}
+	return [-1,1]
+}
+console.log(findEmptySpot(board))
+
+
+
+//check whether a val is in a row
+function checkRow(board,row,val){
+	for(let i=0; i<9; i++){
+		if(board[row][i]===val){
+			return false
+		}
+	}
+	return true
+}
+
+
+//check values in a column
+function checkCol(board,col,val){
+	for(let i=0;i<board.length;i++){
+		if(board[i][col]===val){
+			return false;
+		}
+	}
+	return true;
+}
+
+function checkSquare(board,row,col,val){
+	boxRow=Math.floor(row/3)*3;
+	boxCol=math.floor(col/3)*3;
+	for(let r=0;r<3;r++){
+		for(let c=0;c<3;c++){
+			if(board[boxRow+r][boxCol+c]===val){
+				return false
+			}
+		}
+	}
+	return true
+}
+
+
+
+
+
+
+
+
+
+
+
+
